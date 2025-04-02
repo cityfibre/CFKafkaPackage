31356d46ececa3a218e0db453b080638ac01973d
Will <william.franklin@cityfibre.com>
2025-04-02 17:36:13 +0100

    Servic3Provider

---
+++ src/CFKafkaPackageServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace cityfibre\cfkafkapackage;
+
+
+use cityfibre\cfkafkapackage\Console\Commands\PropertySubscriber;
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+class CFKafkaPackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected bool $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/kafka.php' => config_path('kafka.php'),
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                PropertySubscriber::class,
+            ]);
+        }
+
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/kafka.php', 'kafka');
+    }
+
+}

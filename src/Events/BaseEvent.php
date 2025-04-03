798632b9c8b164d1ccaeed8017adfff060e5ff9b
Will <william.franklin@cityfibre.com>
2025-04-03 11:34:16 +0100

    Fix base Event

--- src/Events/BaseEvent.php
+++ src/Events/BaseEvent.php
@@ -1,13 +1,21 @@
 <?php
 
+namespace cityfibre\cfkafkapackage\Events;
 
-const SFID_VALIDATION = 'required|string|max:18';
-const STRING_VALIDATION = 'required|string|max:255';
-const BOOLEAN_VALIDATION = 'required|boolean';
-const INTEGER_VALIDATION = 'required|integer';
-const ARRAY_VALIDATION = 'required|array';
-const NULLABLE_STRING_VALIDATION = 'string|nullable|max:255';
-const NULLABLE_INTEGER_VALIDATION = 'integer|nullable';
-const NULLABLE_NUMERIC_VALIDATION = 'numeric|nullable';
-const NULLABLE_BOOLEAN_VALIDATION = 'boolean|nullable';
-const NUMERIC_VALIDATION = 'required|numeric';
+
+abstract class BaseEvent {
+    const SFID_VALIDATION = 'required|string|max:18';
+    const STRING_VALIDATION = 'required|string|max:255';
+    const BOOLEAN_VALIDATION = 'required|boolean';
+    const INTEGER_VALIDATION = 'required|integer';
+    const ARRAY_VALIDATION = 'required|array';
+    const NULLABLE_STRING_VALIDATION = 'string|nullable|max:255';
+    const NULLABLE_INTEGER_VALIDATION = 'integer|nullable';
+    const NULLABLE_NUMERIC_VALIDATION = 'numeric|nullable';
+    const NULLABLE_BOOLEAN_VALIDATION = 'boolean|nullable';
+    const NUMERIC_VALIDATION = 'required|numeric';
+
+
+}
+
+
